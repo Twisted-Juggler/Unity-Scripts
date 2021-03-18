a1839b279e2affc3c8daff217c3489408dfc0066
Aidyn <60679920+Twisted-Juggler@users.noreply.github.com>
2021-03-17 22:07:01 -0400

    Update Movement.cs

--- Movement.cs
+++ Movement.cs
@@ -5,54 +5,81 @@ public class Movement : MonoBehaviour
     [Space(5)]
     [Header("Developer tools")]
     
-    // Dev only controlls
-    
-    public bool iGoUpNow = false; // flight
-    public bool vroomVroom = false; // speed
+    [SerializeField] bool flight = false;
+    [SerializeField] bool speed = false;
+    [SerializeField] bool lowGravity = false;
+    [SerializeField] bool superJump = false;
 
 
     [Space(5)]
     [Header("Controllers")]
-    
-    // Controllers
-   
-    public Transform cam;
-    public Transform player;
-    public Rigidbody rb;
-    public Transform orientation;
-    public float gravityPull = 0f;
-    
+
+    [SerializeField] Transform orientation;
+    [SerializeField] Transform cam;
+    [SerializeField] Transform player;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] Rigidbody rb;
+    [SerializeField] float gravity = 32f;
+    [SerializeField] LayerMask collisionLayer;
+
+
     [Space(5)]
     [Header("Player Variables")]
-    
-    // Player Variables
-    
-    public float playerSpeed = 1f;
-    public float jumpHeight = 2;
-    public float playerHeight = 4f;
-    public bool isSprinting;
+
+    [SerializeField] float playerSpeed = 1f;
+    [SerializeField] float jumpHeight = 2;
+    [SerializeField] float playerHeight = 4f;
     
 
     [Space(5)]
     [Header("Movement Variables")]
-   
-    // Movement Variables
-    
-    [SerializeField] private float groundDrag = 6f;
-    [SerializeField] private float groundMultiplier = 2f;
-    [SerializeField] private float airDrag = 2f;
-    [SerializeField] private float airMultiplier = 0.4f;
-    [SerializeField] private float speedMultiplier = 10f;
-    private float x, y;
-    private bool isGrounded;
-    private float horizontalMovement;
-    private float verticalMovement;
+
+    [SerializeField] float distance;
+    [SerializeField] float groundDrag = 6f;
+    [SerializeField] float groundMultiplier = 2f;
+    [SerializeField] float airDrag = 2f;
+    [SerializeField] float airMultiplier = 0.4f;
+    [SerializeField] float speedMultiplier = 10f;
     
 
-    KeyCode jumpKey = KeyCode.Space;
+    /*--- Private Variables ---*/
     
+    float horizontalMovement;
+    float verticalMovement;
+    float groundSphere = 0.4f;
+    float x, y;
+    bool isGrounded;
+
+
     Vector3 MoveDirection;
-    Vector3 playerDown;
+    Vector3 slopeMoveDirection;
+    Vector3 velocity;
+
+    [Space(5)]
+    [Header("Keybinds")]
+
+    [SerializeField] KeyCode jumpKey = KeyCode.Space;
+    [SerializeField] KeyCode sprintKey = KeyCode.LeftControl;
+
+    RaycastHit slopeHit;
+
+    private bool OnSlope()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, out slopeHit, playerHeight / 2 + 0.5f))
+        {
+            if (slopeHit.normal != Vector3.up)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        return false;
+    }
+
 
     // On Start
     void Start()
@@ -62,67 +89,78 @@ public class Movement : MonoBehaviour
     }
 
 
-    // Every Frame
-    void Update()
+    void FixedUpdate()
     {
+        /*Quaternion groundRot = Quaternion.FromToRotation(Vector3.up, GroundNormals(distance, collisionLayer));
+        Quaternion cameraDirection = Quaternion.Euler(0, Camera.main.transform.eulerAngles.y, 0);
+
+        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        transform.position += groundRot * cameraDirection * direction * playerSpeed * Time.fixedDeltaTime;*/
+
+        
+
+        Move();
         Gravity();
-        MoveInput();
         ControlDrag();
+        SpeedControl();
 
-        isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * .05f + 0.1f);
+        velocity.y += gravity * Time.deltaTime;
 
-        if(Input.GetKeyDown(jumpKey) && isGrounded)
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundSphere, collisionLayer);
+
+        if (isGrounded && Input.GetKey(jumpKey))
         {
             Jump();
         }
+            
     }
-
-    void FixedUpdate()
-    {
-        Move();
-    }
-
 
     /*--- Movement ---*/
-    void MoveInput()
+
+    void Move()
     {
         horizontalMovement = Input.GetAxisRaw("Horizontal");
         verticalMovement = Input.GetAxisRaw("Vertical");
 
-        if(vroomVroom == true)
-        {
-            playerSpeed = 100;
-        }
+        MoveDirection = orientation.forward * verticalMovement + orientation.right * horizontalMovement;
 
-        else { }
+        slopeMoveDirection = Vector3.ProjectOnPlane(MoveDirection, slopeHit.normal);
 
-        if(iGoUpNow == true)
-        {
-            rb.useGravity = false;
-            gravityPull = 0f;
-            MoveDirection = cam.forward * verticalMovement + cam.right * horizontalMovement;
-        }
-        
-        else { }
-    }
-
-    void Move()
-    {
-        if (isGrounded)
+        if (isGrounded && !OnSlope())
         {
             rb.AddForce(MoveDirection.normalized * speedMultiplier * playerSpeed, ForceMode.Acceleration);
         }
-
+        
+        else if (OnSlope())
+        {
+            rb.AddForce(slopeMoveDirection.normalized * speedMultiplier * playerSpeed, ForceMode.Acceleration);
+        }
+       
         else
         {
             rb.AddForce(MoveDirection.normalized * speedMultiplier * playerSpeed * airMultiplier, ForceMode.Acceleration);
         }
+
+        
+    }
+
+    private Vector3 GroundNormals(float distance, LayerMask collisionLayer)
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit, distance, collisionLayer))
+            return hit.normal;
+        return Vector3.up;
     }
 
 
     void Jump()
     {
-        rb.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+        rb.AddForce(new Vector3(0, 1, 0) * jumpHeight, ForceMode.Impulse);
+    }
+
+    void SpeedControl()
+    {
+        
+        
     }
 
 
@@ -130,7 +168,7 @@ public class Movement : MonoBehaviour
     /*--- Physics ---*/
     void Gravity()
     {
-        rb.AddForce(Vector3.down * gravityPull, ForceMode.Acceleration);
+        rb.AddForce(Vector3.down * gravity, ForceMode.Acceleration);
     }
 
     void ControlDrag()
@@ -143,6 +181,34 @@ public class Movement : MonoBehaviour
         else
         {
             rb.drag = airDrag;
+        }
+    }
+
+    /*--- Misc ---*/
+
+    void DevTools()
+    {
+        if (speed == true)
+        {
+            playerSpeed = 100;
+        }
+
+        if (flight == true)
+        {
+            rb.useGravity = false;
+            gravity = 0f;
+            MoveDirection = cam.forward * verticalMovement + cam.right * horizontalMovement;
+        }
+
+        if (lowGravity == true)
+        {
+            gravity = 9;
+            jumpHeight = 80;
+        }
+
+        if (superJump == true)
+        {
+            jumpHeight = 200;
         }
     }
 }
